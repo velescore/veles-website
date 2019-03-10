4caf83b812f64d3066d193f294ba1e0cc6666875
mdfkbtc <mdfkbtc@gmail.com>
2019-03-10 14:53:56 +0100

    nav footer

---
+++ js/nav.js
@@ -0,0 +1,58 @@
+document.write('<nav class="navbar fixed-top navbar-expand-lg navbar-darker bg-darker fixed-top">\
+                    <div class="container">\
+            <img src="images/veles.ico" />\
+            <a class="navbar-brand" href="index.html">Veles</a>\
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
+                <span class="navbar-toggler-icon"></span>\
+            </button>\
+            <div class="collapse navbar-collapse" id="navbarResponsive">\
+                <ul class="navbar-nav ml-auto">\
+                    <li class="nav-item">\
+                        <a class="nav-link"  href="about.html">About</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link"  href="download.html">Download</a>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link"  href="roadmap.html">Roadmap</a>\
+                    </li>\
+                    <li class="nav-item dropdown">\
+                        <a class="nav-link dropdown-toggle" href="resources.html" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+                     Explorer\
+                     </a>\
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">\
+                            <a class="dropdown-item" href="http://explorer.network">Search</a>\
+                            <a class="dropdown-item" href="http://explorer.network/movement">Movements</a>\
+                            <a class="dropdown-item" href="http://explorer.network/network">Network</a>\
+                            <a class="dropdown-item" href="http://explorer.network/richlist">Richlist</a>\
+                            <a class="dropdown-item" href="http://explorer.network/masternodes">Masternodes</a>\
+                            <a class="dropdown-item" href="http://explorer.network/coininfo">Coin-info</a>\
+                            <a class="dropdown-item" href="http://explorer.network/mining-stats">Mining-stats</a>\
+                            <a class="dropdown-item" href="http://explorer.network/api">Api</a>\
+                        </div>\
+                    </li>\
+                    <li class="nav-item dropdown">\
+                        <a class="nav-link dropdown-toggle" href="resources.html" id="navbarDropdownBlog" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+                     Resources\
+                     </a>\
+                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownBlog">\
+                             <a class="dropdown-item" href="faq.html">FAQ</a>\
+                             <a class="dropdown-item" href="wiki.html">Wiki</a>\
+                             <a class="dropdown-item" href="coin-specs.html">Coin-specs</a>\
+                             <a class="dropdown-item" href="mining-pools.html">Mining-pools</a>\
+                             <a class="dropdown-item" href="services.html">External-services</a>\
+                             <a class="dropdown-item" href="community.html">Community-milestones</a>\
+                         </div>\
+                    </li>\
+                    <li class="nav-item">\
+                        <a class="nav-link" href="contact.html">Contact</a>\
+                    </li>\
+                    <li>\
+                        <div id="menu-primary"></div>\
+                    </li>\
+            </div>\
+            </ul>\
+        </div>\
+        </div>\
+    </nav>\
+    ');
