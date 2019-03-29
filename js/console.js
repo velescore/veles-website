2e0e6d9c4a024cbe3c1866b57c52a33f0161cb7f
AltcoinBaggins <marian@priv.profiweb.biz>
2019-03-29 15:56:45 +0100

    Add support for internal commands (.clear, .help) to dev console

--- js/console.js
+++ js/console.js
@@ -7,7 +7,8 @@ var velesDevConsole = {
     cmdResult: false,
     welcomeMessage: "Veles Core Websocket client version v0.17 \"Aged Amnesia\"\n"
         + " ____   ____     .__                _________  \n_\\___\\_/___/____ |  |   ____   _____\\_   ___ \\  ___________   ____  \n\\___________/__ \\|  | _/ __ \\ /  ___/    \\  \\/ /  _ \\_  __ \\_/ __ \\ \n   \\  Y  /\\  ___/|  |_\\  ___/ \\___ \\\\     \\___(  <_> )  | \\/\\  ___/ \n    \\___/  \\___  >____/\\___  >____  >\\______  /\\____/|__|    \\___  >\n               \\/          \\/     \\/        \\/                   \\/ \n"
-        + "This feature is highly EXPERIMENTAL, please use at your own risk.\n",
+        + "This feature is highly EXPERIMENTAL, please use at your own risk.\n"
+        + "Type .help for more information about available commands.\n\n",
 
     init: function(websocketClient) {
         if (!this.isInitialized) {
@@ -29,13 +30,17 @@ var velesDevConsole = {
                         if (line == "") 
                             return true;
 
-                        velesDevConsole.handleWalletCommand(line, report);
+                        if (line[0] == '.')
+                            velesDevConsole.handleInternalCommand(line, report);
+                        else
+                            velesDevConsole.handleWalletCommand(line, report);
                     },
                     animateScroll: false,
                     promptHistory: true,
                     autofocus: true,
                     welcomeMessage: velesDevConsole.welcomeMessage
                 });
+                velesDevConsole.controller.promptText('.help');
             }); 
             // auto-focus when mouse over console
             $('#dev-console').mouseover(function() {
@@ -49,21 +54,47 @@ var velesDevConsole = {
     },
 
     handleInternalCommand: function(line, report) {
-        return '[internal]';
+        args = line.split(' ');
+        cmd = line.split(' ')[0];
+
+        if (cmd == '.clear') {
+            report('');
+            return velesDevConsole.controller.clearScreen();
+
+        } else if (cmd == '.help') {
+            return report(""
+                + "== Internal console commands ==\n"
+                + ".clear\n.help\n\n"
+                + "== Veles Core daemon commands ==\n"
+                + "help\ngethalvingstatus\ngetmultialgostatus\ngetblockchaininfo\n...\n"
+                + "For a full list of Veles Core daemon commands please run 'help'\n\n"
+                );
+        }
+
+        return velesDevConsole.handleCommandNotFound(line, report);
     },
 
     handleWalletCommand: function(line, report) {
         this.client.get_cmd_result('node', line, {}, function(data) {
-            parts = line.split(' ');
-
             if (data == null || data == false)  // depends on current API impl.
-                return report(parts[0] + ': command not found');
+                return velesDevConsole.handleCommandNotFound(line, report);
 
             if (typeof data == 'object')
                 report(JSON.stringify(data, null, 4));
             else
                 report(data.toString());
         });
+    },
+
+    handleCommandNotFound(line, report) {
+        cmdName = line.split(' ')[0];
+        cmdSuggestion = null;
+
+        if (cmdName == 'clear')
+            cmdSuggestion = "Command '.clear' from console internal commands";
+
+        report(((cmdSuggestion) ? "No command '" + cmdName + "' found, did you mean:\n " + cmdSuggestion + "\n"  : '')
+            + cmdName + ': command not found');
     },
 
     show: function() {
