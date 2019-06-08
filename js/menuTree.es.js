8503877c58c4cc58fdd54a49ae77e9985eb01cc8
mdfkbtc <mdfkbtc@gmail.com>
2019-06-08 16:18:04 +0200

    FIX: menutree ...

---
+++ js/menuTree.es.js
@@ -0,0 +1,104 @@
+/*
+ * Veles Website sctucture in js/json format.
+ *
+ * Copyright (C) 2019 The Veles Core developers
+ * Author: Altcoin Baggins
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ */
+var menuTree = [
+	{
+		'page': 'index',
+			'title': 'Home',
+			'sections': [
+		  { 'title': 'Getting Started' },
+		  { 'title': 'Restoring Privacy' },
+		  { 'title': 'Innovative Network' },
+		  { 'title': 'Funding and Launch' },
+			{ 'title': 'Economic Model' },
+			{ 'title': 'Open Governance' },
+			 { 'title': 'Main Features' }
+		],
+		'hideFromNav': true,
+	},
+	{
+		'page': 'roadmap',
+      'title': 'Roadmap'
+      'sections': [
+			{ 'title': 'Q1' },
+			{ 'title': 'Q2' },
+			{ 'title': 'Q3' },
+			 { 'title': 'Q4' }
+		]
+	},
+	{
+    'page': 'team',
+  		'title': 'Team',
+  		'sections': [
+  		{ 'title': 'About-us' },
+  		{ 'title': 'AltcoinBaggins' },
+  		{ 'title': 'Mdfkbtc' },
+  		{ 'title': 'Uhlik' },
+  		{ 'title': 'Virtuado' },
+  		 { 'title': 'Maty' }
+		]
+	},
+	{
+    'page': 'download',
+  		'title': 'Download',
+  		'sections': [
+  		  { 'title': 'Linux Wallet' },
+  			{ 'title': 'Windows Wallet' },
+  			{ 'title': 'macOS Wallet' },
+  			{ 'title': 'RaspberryPi Wallet' },
+  			 { 'title': 'Source-code' }
+		]
+	},
+	{
+    'page': 'resources'
+  		'title': 'Resources',
+  		'items': [
+  			{ 'title': 'Coin-specs' },
+  			{ 'title': 'Whitepaper' },
+  			{ 'title': 'Masternode-guide' },
+  			{ 'title': 'Exchanges' },
+  			{ 'title': 'Mining-pools'},
+  			{ 'title': 'External-services'},
+  			{ 'title': 'Crowdfundings'},
+  			{ 'title': 'FAQ' }
+		]
+	},
+	{
+    'page': 'Explorer',
+  		'title': 'Explorer',
+  		'items': [
+  			{
+  				'title': 'Index',
+  			  	'url': 'https://explorer.veles.network'
+  			},
+  			{
+  				'title': 'Richlist',
+  				'url': 'https://explorer.veles.network/Richlist'
+  			},
+  			{
+  				'title': 'Masternodes',
+  				'url': 'https://explorer.veles.network/masternodes'
+  			},
+  			{
+  				'title': 'Movement',
+  				'url': 'https://explorer.veles.network/movement'
+  			},
+  			{
+  				'title': 'Mining-stats',
+  				'url': 'https://explorer.veles.network/miningstats'
+  			},
+  			{
+  				'title': 'Api',
+  				'url': 'https://explorer.veles.network/info'
+  			}
+		]
+	}
+];
