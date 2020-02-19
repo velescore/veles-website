d42e3ad4b70af2e3eff2f28c5c1502d337f0868a
AltcoinBaggins <marian@priv.profiweb.biz>
2020-02-19 09:43:24 +0100

    Update menu-tree to accomodate wiki

--- public/js/menuTree.js
+++ public/js/menuTree.js
@@ -10,6 +10,135 @@
  * of the License, or (at your option) any later version.
  */
 var menuTree = [
+/* Sections visible on navigation menu */
+	{
+		'title': 'Roadmap',
+		'sections': [
+			{ 'title': 'Q1' },
+			{ 'title': 'Q2' },
+			{ 'title': 'Q3' },
+			{ 'title': 'Q4' }
+		]
+	},
+	{
+		'title': 'Team',
+		'sections': [
+			{ 'title': 'About Us' },
+			{ 'title': 'AltcoinBaggins' },
+			{ 'title': 'Mdfkbtc' },
+			{ 'title': 'Uhlik' },
+			{ 'title': 'Virtuado' },
+			{ 'title': 'Maty' },
+					{ 'title': 'AlexDeLarge' },
+				{ 'title': 'Clover' }
+		]
+	},
+	{
+		'title': 'Download',
+		'sections': [
+			{ 'title': 'Linux Wallet' },
+			{ 'title': 'Windows Wallet' },
+			{ 'title': 'macOS Wallet' },
+			{ 'title': 'RaspberryPi Wallet' },
+			{ 'title': 'Source-code' }
+		]
+	},
+	{
+		'title': 'Wiki',
+		'sidebarCaption': 'Pinned Articles',
+		'items': [
+			{ 
+				'title': 'Wiki Homepage', 
+				'page': 'Home.wiki' 
+			},
+			{ 
+				'title': 'Coin Specifications', 
+				'page': 'Coin-Specifications.wiki' 
+			},
+			{ 
+				'title': 'Whitepaper', 
+				'page': 'Whitepaper.wiki' 
+			},
+			{ 
+				'title': 'dVPN Setup Guide', 
+				'page': 'dVPN-Setup-Guide.wiki' 
+			},
+			{ 
+				'title': 'Masternode Setup Guide', 
+				'page': 'Masternode-Setup-Guide.wiki' 
+			},
+			{ 
+				'title': 'Exchanges', 
+				'page': 'Exchanges.wiki' 
+			},
+			{ 
+				'title': 'Mining Pools', 
+				'page': 'Mining-Pools.wiki' 
+			},
+			{ 
+				'title': 'External Services', 
+				'page': 'External-Services.wiki' 
+			}
+		]
+	},
+	{
+		'title': 'Wallet',
+		'items': [
+			{
+				'title': 'Go to WebWallet',
+					'url': 'https://wallet.veles.network'
+			},
+			{
+				'title': 'Login',
+				'url': 'https://wallet.veles.network/#wallet'
+			},
+			{
+				'title': 'Generate',
+				'url': 'https://wallet.veles.network/#newAddress'
+			},
+			{
+				'title': 'Verify',
+				'url': 'https://wallet.veles.network/#verify'
+			},
+			{
+				'title': 'Sign',
+				'url': 'https://wallet.veles.network/#sign'
+			},
+			{
+				'title': 'Broadcast',
+				'url': 'https://wallet.veles.network/#broadcast'
+			}
+		]
+	},
+	{
+		'title': 'Explorer',
+		'items': [
+			{
+				'title': 'Go to Explorer',
+				'url': 'https://explorer.veles.network'
+			},
+			{
+				'title': 'Richlist',
+				'url': 'https://explorer.veles.network/Richlist'
+			},
+			{
+				'title': 'Masternodes',
+				'url': 'https://explorer.veles.network/masternodes'
+			},
+			{
+				'title': 'Movement',
+				'url': 'https://explorer.veles.network/movement'
+			},
+			{
+				'title': 'Mining-stats',
+				'url': 'https://explorer.veles.network/miningstats'
+			},
+			{
+				'title': 'Api',
+				'url': 'https://explorer.veles.network/info'
+			}
+		]
+	},
 /* Hidden special sections */
 	{
 		'page': 'index',
@@ -31,7 +160,9 @@ var menuTree = [
 		'hideFromNav': true,
 		'itemsFromJsonID': 'news-pages-json'
 	},
-/* Sections visible on navigation menu */
+	{ 'page': 'crowdfundings', 'title': 'Crowdfundings', 'hideFromNav': true},
+	{ 'page': 'faq', 'title': 'FAQ', 'hideFromNav': true }
+/*
 	{
 		'title': 'dVPN',
 		'items': [
@@ -89,129 +220,5 @@ var menuTree = [
 				}
 			]
 		},
-	{
-		'title': 'Roadmap',
-		'sections': [
-			{ 'title': 'Q1' },
-			{ 'title': 'Q2' },
-			{ 'title': 'Q3' },
-			{ 'title': 'Q4' }
-		]
-	},
-	{
-		'title': 'Team',
-		'sections': [
-			{ 'title': 'About Us' },
-			{ 'title': 'AltcoinBaggins' },
-			{ 'title': 'Mdfkbtc' },
-			{ 'title': 'Uhlik' },
-			{ 'title': 'Virtuado' },
-			{ 'title': 'Maty' },
-					{ 'title': 'AlexDeLarge' },
-				{ 'title': 'Clover' }
-		]
-	},
-	{
-		'title': 'Download',
-		'sections': [
-			{ 'title': 'Linux Wallet' },
-			{ 'title': 'Windows Wallet' },
-			{ 'title': 'macOS Wallet' },
-			{ 'title': 'RaspberryPi Wallet' },
-			{ 'title': 'Source-code' }
-		]
-	},
-	{
-		'title': 'Resources',
-		'items': [
-			{ 'title': 'Coin Specs' },
-			{ 'title': 'Whitepaper PDF' },
-			{ 'title': 'Masternode Guide PDF' },
-			{ 'title': 'Exchanges' },
-			{ 'title': 'Mining Pools'},
-			{ 'title': 'External Services'},
-			{ 'title': 'Crowdfundings'},
-			{ 'title': 'FAQ' }
-		]
-	},
-	{
-		'title': 'Wiki',
-		'sidebarCaption': 'Pinned Articles',
-		'items': [
-			{ 
-				'title': 'Index', 
-				'page': 'Home.wiki' 
-			},
-			{ 
-				'title': 'Masternode Setup Guide', 
-				'page': 'Masternode-Setup-Guide.wiki' 
-			},
-			{ 
-				'title': 'dVPN Setup Guide', 
-				'page': 'dVPN-Setup-Guide.wiki' 
-			},
-			{ 
-				'title': 'Whitepaper', 
-				'page': 'Whitepaper.wiki' 
-			}
-		]
-	},
-	{
-		'title': 'Wallet',
-		'items': [
-			{
-				'title': 'Index',
-					'url': 'https://wallet.veles.network'
-			},
-			{
-				'title': 'Login',
-				'url': 'https://wallet.veles.network/#wallet'
-			},
-			{
-				'title': 'Generate',
-				'url': 'https://wallet.veles.network/#newAddress'
-			},
-			{
-				'title': 'Verify',
-				'url': 'https://wallet.veles.network/#verify'
-			},
-			{
-				'title': 'Sign',
-				'url': 'https://wallet.veles.network/#sign'
-			},
-			{
-				'title': 'Broadcast',
-				'url': 'https://wallet.veles.network/#broadcast'
-			}
-		]
-	},
-	{
-		'title': 'Explorer',
-		'items': [
-			{
-				'title': 'Index',
-					'url': 'https://explorer.veles.network'
-			},
-			{
-				'title': 'Richlist',
-				'url': 'https://explorer.veles.network/Richlist'
-			},
-			{
-				'title': 'Masternodes',
-				'url': 'https://explorer.veles.network/masternodes'
-			},
-			{
-				'title': 'Movement',
-				'url': 'https://explorer.veles.network/movement'
-			},
-			{
-				'title': 'Mining-stats',
-				'url': 'https://explorer.veles.network/miningstats'
-			},
-			{
-				'title': 'Api',
-				'url': 'https://explorer.veles.network/info'
-			}
-		]
-	}
+*/
 ];
